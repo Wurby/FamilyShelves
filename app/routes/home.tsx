4c45e15aa2b67f4c10dcea1402ce8e0d5c70f675
Joshua Pearson <thejoshuapearson@icloud.com>
2025-01-27 18:40:03 -0700

    current

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Housemaster" },
+    { name: "description", content: "Welcome to Housemaster" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Home</div>;
 }
